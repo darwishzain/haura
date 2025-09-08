5e701cf6c4c6810480e9a90be4a850a88736a52c
Darwish Mat Zain - Hidari Shoya <darwishzainstudio@gmail.com>
2025-09-08 19:23:32 +0800

    something something corpus

--- haura.py
+++ haura.py
@@ -1,20 +1,7 @@
-#import os
-#import datetime
-import pyttsx3
+import json
 
-engine = pyttsx3.init()
-engine.say("I will speak this text")
-engine.runAndWait()
-
-
-
-
-
-
-
-#def tts(text):
-#      return os.system("espeak  -s 155 -a 200 "+text+" " )
-
-#m = datetime.datetime.now().strftime("%I %M %S")
-#tts("'Sir the time is"+str(int(m[0:2]))+" "+str(int(m[3:5]))+" : ' ")
-#tts("morning"+"sir")
+with open('corpus/dialogue/greetings.jsonl') as f:
+    for line in f:
+        item = json.loads(line)
+        print(f"User: {item['user']}")
+        print(f"HAURA: {item['haura']}")
